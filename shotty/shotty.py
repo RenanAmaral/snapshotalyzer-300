49e52e5b36cab19fd9572a4f4661ee4549e43501
Renan <renan.nan.amaral@hotmail.com>
2020-11-05 16:44:03 -0300

    Final Version

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -2,14 +2,19 @@ import boto3
 import botocore
 import click
 
-session = boto3.Session(profile_name='shotty')
-ec2 = session.resource('ec2')
+def set_profile(profile_name):
+    session = boto3.Session(profile_name=profile_name)
+    ec2 = session.resource('ec2')
 
+    return ec2
 
-def filter_instances(project):
+def filter_instances(project, instance):
     instances = []
 
-    if project:
+    if instance:
+        filters = [{'Name': 'instance-id', 'Values':[instance]}]
+        instances = ec2.instances.filter(Filters=filters)
+    elif project:
         filters = [{'Name': 'tag:Project', 'Values':[project]}]
         instances = ec2.instances.filter(Filters=filters)
     else:
@@ -22,8 +27,11 @@ def has_pending_snapshot(volume):
     return snapshots and snapshots[0].state == 'pending'
 
 @click.group()
-def cli():
+@click.option('--profile', 'profile_name', default='shotty', help="Specify a profile")
+def cli(profile_name):
+
     """Shotty manage snapshots"""
+    set_profile(profile_name)
 
 @cli.group('snapshots')
 def snapshots():
@@ -32,11 +40,13 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help="Only snapshots for project (tag Project:<name>)")
+@click.option('--instance', default=None,
+    help="Only specified instance")
 @click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for each volume, not just most recent")
-def list_snapshots(project, list_all):
+def list_snapshots(project, list_all, instance):
     "List EC2 snapshots"
 
-    instances = filter_instances(project)
+    instances = filter_instances(project, instance)
 
     for i in instances:
         for v in i.volumes.all():
@@ -61,10 +71,12 @@ def volumes():
 @volumes.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
-def list_volumes(project):
+@click.option('--instance', default=None,
+    help="Only specified instance")
+def list_volumes(project, instance):
     "List EC2 volumes"
 
-    instances = filter_instances(project)
+    instances = filter_instances(project, instance)
 
     for i in instances:
         for v in i.volumes.all():
@@ -86,12 +98,20 @@ def instances():
 @instances.command('snapshot', help="Create snapshots of all volumes")
 @click.option('--project', default=None,
     help="Only volumes for project (tag Project:<name>)")
-def create_snapshots(project):
+@click.option('--instance', default=None,
+    help="Only specified instance")
+@click.option('--force', 'force', default=False, is_flag=True, help="Force snapshots for all instances")
+def create_snapshots(project, force, instance):
     "Create snapshots for EC2 instances"
 
-    instances = filter_instances(project)
+    if not project and not force:
+        raise Exception("Choose a project or use --force option to apply for all instances")
+        exit()
+
+    instances = filter_instances(project, instance)
 
     for i in instances:
+        last_state = i.state['Name']
         print("Stopping {0}".format(i.id))
         i.stop()
         i.wait_until_stopped()
@@ -101,12 +121,19 @@ def create_snapshots(project):
                 continue
 
             print("Creating snapshot of {0}".format(v.id))
-            v.create_snapshot(Description="Created by SnapshotAlyzer 3000")
 
-        print("Starting {0}".format(i.id))
+            try:
+                v.create_snapshot(Description="Created by SnapshotAlyzer 3000")
+            except botocore.exceptions.ClientError as e:
+                print(" Could not snapshot {0}. ".format(v.id) + str(e))
+                continue
 
-        i.start()
-        i.wait_until_running()
+        if last_state == 'running':
+            print("Starting {0}".format(i.id))
+            i.start()
+            i.wait_until_running()
+        else:
+            continue
 
     print("Job's done!")
 
@@ -115,10 +142,12 @@ def create_snapshots(project):
 @instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
-def list_instances(project):
+@click.option('--instance', default=None,
+    help="Only specified instance")
+def list_instances(project, instance):
     "List EC2 instances"
 
-    instances = filter_instances(project)
+    instances = filter_instances(project, instance)
 
     for i in instances:
         tags = {t['Key']: t['Value'] for t in i.tags or []}
@@ -134,10 +163,17 @@ def list_instances(project):
 @instances.command('stop')
 @click.option('--project', default=None,
     help='Only instances for project')
-def stop_instances(project):
+@click.option('--force', 'force', default=False, is_flag=True, help="Force stop for all instances")
+@click.option('--instance', default=None,
+    help="Only specified instance")
+def stop_instances(project, force, instance):
     "Stop EC2 Instances"
 
-    instances = filter_instances(project)
+    if not project and not force:
+        raise Exception("Choose a project or use --force option to apply for all instances")
+        exit()
+
+    instances = filter_instances(project, instance)
 
     for i in instances:
         print("Stopping {0}..".format(i.id))
@@ -150,10 +186,17 @@ def stop_instances(project):
 @instances.command('start')
 @click.option('--project', default=None,
     help='Only instances for project')
-def start_instances(project):
+@click.option('--force', 'force', default=False, is_flag=True, help="Force start for all instances")
+@click.option('--instance', default=None,
+    help="Only specified instance")
+def start_instances(project, force, instance):
     "Start EC2 Instances"
 
-    instances = filter_instances(project)
+    if not project and not force:
+        raise Exception("Choose a project or use --force option to apply for all instances")
+        exit()
+
+    instances = filter_instances(project, instance)
 
     for i in instances:
         print("Starting {0}..".format(i.id))
@@ -163,5 +206,30 @@ def start_instances(project):
             print(" Could not start {0}. ".format(i.id) + str(e))
             continue
 
+@instances.command('reboot')
+@click.option('--project', default=None,
+    help='Only instances for project')
+@click.option('--force', 'force', default=False, is_flag=True, help="Force reboot for all instances")
+@click.option('--instance', default=None,
+    help="Only specified instance")
+def reboot_instances(project, force, instance):
+    "Reboot EC2 Instances"
+
+    if not project and not force:
+        raise Exception("Choose a project or use --force option to apply for all instances")
+        exit()
+
+    instances = filter_instances(project, instance)
+
+    for i in instances:
+        print("Rebooting {0}..".format(i.id))
+        try:
+            i.reboot()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not reboot {0}. ".format(i.id) + str(e))
+            continue
+
 if __name__ == '__main__':
+
+    ec2 = set_profile('shotty')
     cli()
